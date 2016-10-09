ebc940c4dc7c88c259c8862ceaa496f1a512f69e
jiboxue <jibx@ichsy.com>
2016-10-09 15:26:18 +0800

    Initial Commit

---
+++ PPX_CircularProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPX_CircularProgress
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PPX_CircularProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PPX_CircularProgress
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
