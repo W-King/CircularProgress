2b8531602ef6a37ca6429beafa96c37e2c61ceb2
jiboxue <jibx@ichsy.com>
2016-10-09 16:57:36 +0800

    first commit

---
+++ PPX_CircularProgress/PPX_CircularProgressView/PPX_CircularProgressView.h
@@ -0,0 +1,13 @@
+//
+//  PPX_CircularProgressView.h
+//  PPX_CircularProgress
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPX_CircularProgressView : UIView
+
+@end
---
+++ PPX_CircularProgress/PPX_CircularProgressView/PPX_CircularProgressView.m
@@ -0,0 +1,21 @@
+//
+//  PPX_CircularProgressView.m
+//  PPX_CircularProgress
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import "PPX_CircularProgressView.h"
+
+@implementation PPX_CircularProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
